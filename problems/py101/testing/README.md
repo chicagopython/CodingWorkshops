425051dcfff5deb9e2b72e15f0e2d07b3401269d
Tathagata <tathagatadg@gmail.com>
2017-08-17 07:36:11 -0500

    initial commit of testing

---
+++ problems/py101/testing/README.md
@@ -0,0 +1,59 @@
+# Intro to PyTest
+This is a gentle introduction to testing with pytest.
+We will use the problem that we have seen before
+at project night, how to group people into teams of 4.
+
+team_organizer.py is simplified implementation of the
+problem.
+
+test_team_organizer.py is the test for the module.
+It has one test implemented to show you what a unit
+test in pytest looks like. You wull need to implemet
+the rest of the tests. Feel free to add more tests.
+
+Documentation: https://docs.pytest.org/en/latest/
+
+# Installation instruction
+This exercise is Python 3 only. Follow instructions on the
+main wiki to see how to install python3.
+
+
+First create a virtual environment
+
+    python3 -m venv venv
+    source venve/bin/activate
+
+
+To install the requirements run
+
+    pip3 install -r requirements.txt
+
+This will install pytest and other required pacakages.
+
+# Running the tests
+
+Simply run pytest at the command prompt.
+This will run the tests in the test_team_organizer.py
+
+By default pytest will capture everything that goes into
+stdout, so that it can have better control over what is
+printed out. If you are adding print statements to see
+what values your variables have run
+
+    pytest -s
+
+While we do not go into much depth of what can be achived by
+pytest (coming soon to a future project night), here is one cool 
+feature you will enjoy.
+
+Say you have hundreds of tests (unlikley for this
+exercise), you can easily parallelize those tests by
+
+    pytest -n3
+
+Where 3 is the number of cpus you want to distribute
+your tests to.
+
+
+# TODO 
+Add virtual env for windows
---
+++ problems/py101/testing/team_orgranizer.py
@@ -0,0 +1,77 @@
+import cmd
+import collections
+
+Person = collections.namedtuple('Person', 'name slackhandle lines')
+
+
+def parse(arg):
+    try:
+        name, slackhandle, lines = arg.split()
+        return Person(name, slackhandle, int(lines))
+    except ValueError:
+        print("Invalid entry format")
+
+
+class Organizer:
+    def __init__(self):
+        self.d = collections.deque()
+
+    def median_lines(self):
+        return self.d[len(self.d)//2].lines
+
+    def add(self, p):
+        '''Takes a person and adds it to the queue
+        if the number of lines is higher than the median
+        it adds to the end else it adds to the front '''
+
+        if len(self.d) == 0:
+            self.d.append(p)
+            return
+
+        if self.median_lines() >= p.lines:
+            self.d.appendleft(p)
+        else:
+            self.d.append(p)
+
+    def _names(self, persons):
+        return ', '.join(p.name for p in persons)
+
+    def teams(self):
+        ''' Makes teams by taking 2 from the front of the queue 
+            and two from the end of the queue till teams of 4
+            can be made. '''
+        teams = []
+        while len(self.d) >= 4:
+            ps = self.d.popleft(), self.d.popleft(), self.d.pop(), self.d.pop()
+            teams.append([self._names(ps)])
+
+        if self.d:
+            teams.append(self._names(self.d))
+            self.d.clear()
+        return teams
+
+
+class OrganizerShell(cmd.Cmd):
+    intro = 'Welcome to Chicago Python Project Night Team Organizer'
+    prompt = 'org> '
+
+    def __init__(self):
+        cmd.Cmd.__init__(self)
+        self.org = Organizer()
+
+    def do_add(self, arg):
+        'Adds a new user. Needs Name, slackhandle, number of lines'
+        p = parse(arg)
+        self.org.add(p)
+
+    def do_print(self, arg):
+        'Print the team organization'
+        for team in self.org.teams():
+            print(team)
+
+    def do_exit(self, args):
+        return True
+
+
+if __name__ == '__main__':
+    OrganizerShell().cmdloop()
